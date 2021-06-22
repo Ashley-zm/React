9e75398b7ca522eb229508dec0065ab3f33d4c5a
朱梅 <1540588534@qq.com>
2021-06-22 10:23:04 +0800

    note

--- react-study/src/App.jsx
+++ react-study/src/App.jsx
@@ -1,17 +1,13 @@
 import React, { Component } from 'react'
 import { Button } from 'antd'
-import { DownloadOutlined } from '@ant-design/icons'
-import 'antd/dist/antd.css'
+// import { DownloadOutlined } from '@ant-design/icons'
+import 'antd/dist/antd.less'
+import './App.less'
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Button
-          type="primary"
-          shape="round"
-          icon={<DownloadOutlined />}
-          size="large"
-        />
+        <Button type="primary">Primary Button</Button>
       </div>
     )
   }
